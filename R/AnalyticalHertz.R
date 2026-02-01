69d7c473d068cd11fdd375b1aa255870f8d21d80
Greg <ghertzlerau@gmail.com>
2026-02-01 13:53:59 +0800

    Put b and c on labels of option plots

--- R/AnalyticalHertz.R
+++ R/AnalyticalHertz.R
@@ -4052,6 +4052,8 @@ Analytical <- R6::R6Class("Analytical",
       y <- private$x_stoch_args[[4]]
       r <- private$x_stoch_args[[5]]
       phi <- private$x_stoch_args[[6]]
+      b <- private$x_stoch_args[[7]]
+      c <- private$x_stoch_args[[8]]
       type <- private$plot_info$plottype$type
       font <- list(family=private$plot_info$plotfont$family,size=private$plot_info$plotfont$size,color=private$plot_colors$font)
       file <- private$plot_info$plotfile
@@ -4090,7 +4092,8 @@ Analytical <- R6::R6Class("Analytical",
         esml <- "</span>"
         bsym <- "<span style='font-family: Symbol;'>"
         esym <- "</span>"
-        syms <- paste(sep="","(<i>t</i>",bsml,bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",",bsym,"<i>s</i>=",esym,format(sigma,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),")",esml)
+        if(phi > 0) { syms <- paste(sep="",bsml,"(<i>t</i>",bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>b</i>",bsym,"=",esym,format(b,digits=4),")",esml) }
+        else { syms <- paste(sep="",bsml,"(<i>t</i>",bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>c</i>",bsym,"=",esym,format(c,digits=4),")",esml) }
         if(is.null(title)) { title <- "Option" }
       }
       else if(is.null(title)) { title <- "" }
@@ -4272,7 +4275,8 @@ Analytical <- R6::R6Class("Analytical",
         esml <- "</span>"
         bsym <- "<span style='font-family: Symbol;'>"
         esym <- "</span>"
-        syms <- paste(sep="",bsml,"(<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",",bsym,"<i>s</i>=",esym,format(sigma,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),")",esml)
+        if(phi > 0) { syms <- paste(sep="",bsml,"(<i>t</i>",bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>b</i>",bsym,"=",esym,format(b,digits=4),")",esml) }
+        else { syms <- paste(sep="",bsml,"(<i>t</i>",bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>c</i>",bsym,"=",esym,format(c,digits=4),")",esml) }
         if(is.null(title)) { title="Option Envelope"  }
       }
       else if(is.null(title)) { title="" }
@@ -4465,7 +4469,8 @@ Analytical <- R6::R6Class("Analytical",
       esym <- "</span>"
       if(labels == TRUE)
       {
-        syms <- paste(sep="",bsml,"(<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",",bsym,"<i>s</i>=",esym,format(sigma,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),")",esml)
+        if(phi > 0) { syms <- paste(sep="",bsml,"(<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>b</i>",bsym,"=",esym,format(b,digits=4),")",esml) }
+        else { syms <- paste(sep="",bsml,"(<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>c</i>",bsym,"=",esym,format(c,digits=4),")",esml) }
         if(is.null(title)) { title <- "Decision Threshold" }
         if(is.null(xaxis)) { xaxis <- paste(sep="","<i>x</i><br>",syms) }
         else{ xaxis <- paste(sep="",xaxis,"<br>",syms) }
@@ -4591,7 +4596,7 @@ Analytical <- R6::R6Class("Analytical",
         esml <- "</span>"
         bsym <- "<span style='font-family: Symbol;'>"
         esym <- "</span>"
-        syms <- paste(sep="","(<i>t</i>",bsml,bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),")",esml)
+        syms <- paste(sep="",bsml,"(<i>t</i>",bsym,"=",esym,format(t,digits=4),",<i>y</i>",bsym,"=",esym,format(y,digits=4),",",bsym,"<i>r</i>=",esym,format(rho,digits=4),",",bsym,"<i>m</i>=",esym,format(mu,digits=4),",<i>r</i>",bsym,"=",esym,format(r,digits=4),",",bsym,"<i>f</i>=",esym,format(phi,digits=4),",<i>b</i>",bsym,"=",esym,format(b,digits=4),",<i>c</i>",bsym,"=",esym,format(c,digits=4),")",esml)
         if(is.null(title))
         {
           if(phi > 0) { title <- "Prohibition" }
