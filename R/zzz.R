b5c5b3959be07a0ffe7d1258d9d3e530ac6ec94d
Greg <ghertzlerau@gmail.com>
2026-09-20 16:52:28 +0800

    Thread enquiry still

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,12 @@ NULL
 
 library(RcppParallel)
 
+#' @rdname OptionalPackages
+#' @usage  RcppParallelThreads()
+#' @return int <- RcppParallelThreads()
+#' @export
+RcppParallelThreads = function() { return(RcppParallel::defaultNumThreads()) }
+
 .onAttach <- function(libname,pkgname) {
     msg <- paste0("\nGreetings!\n",
         "\u2139\ufe0f OUPShiny() to launch the RShiny app,\n",
@@ -16,7 +22,7 @@ library(RcppParallel)
         "\u2139\ufe0f OUPDataList() of data sets,\n",
         "\u2139\ufe0f OUPDemoList() of demos.\n\n",
         "\u2705 Compiled with Rcpp, ")
-    if (RcppParallelInstalled()) {  parallel <- paste0("with RcppParallel (",RcppParallel::defaultNumThreads()," threads), ") }
+    if (RcppParallelInstalled()) {  parallel <- paste0("with RcppParallel (",RcppParallelThreads()," threads), ") }
     else { parallel <- "without RcppParallel, "}
     if (RcppdqrngInstalled()) { dqrng <- "with dqrng, " }
     else { dqrng <- "without dqrng, " }
